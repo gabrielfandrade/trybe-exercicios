fae6b0669d156304ddd7763508691561bde84401
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-03-08 13:20:42 -0300

    For

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/fixar.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/fixar.js
@@ -13,3 +13,9 @@
 // menu.push("Contato");
 
 // console.log(menu);
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index++) {
+    console.log(groceryList[index]);
+}
