073f1f55b9c0e188e6b40e88db9705d5bf7549cb
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-03-08 13:42:22 -0300

    Maior numero no Array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -2,18 +2,28 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(numbers);
 
-let soma = 0;
+// let soma = 0;
 
-for (const number of numbers) {
-    soma += number;
-}
+// for (const number of numbers) {
+//     soma += number;
+// }
 
 // console.log(soma);
 
 // console.log(soma / numbers.length);
 
-if (soma > 20) {
-    console.log("valor maior que 20");
-} else {
-    console.log("valor menor ou igual a 20");
+// if (soma > 20) {
+//     console.log("valor maior que 20");
+// } else {
+//     console.log("valor menor ou igual a 20");
+// }
+
+let maiorNumero = 0;
+
+for (let number of numbers) {
+    if (number > maiorNumero) {
+        maiorNumero = number;
+    }
 }
+
+console.log(maiorNumero);
