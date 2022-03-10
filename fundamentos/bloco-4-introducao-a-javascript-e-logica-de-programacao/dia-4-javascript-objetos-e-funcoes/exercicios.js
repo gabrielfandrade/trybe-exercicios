c6688a7982fc895ccc814b863dacf9290b3e3c9a
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-03-10 14:45:06 -0300

    Exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -13,6 +13,22 @@ let info = {
 //     console.log(key);
 // }
 
-for (const key in info) {
-    console.log(info[key]);
+// for (const key in info) {
+//     console.log(info[key]);
+// }
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  };
+
+console.log(info['personagem'],'e',info2['personagem']);
+console.log(info['origem'],'e',info2['origem']);
+console.log(info['nota'],'e',info2['nota']);
+if (info['recorrente']==info2['recorrente']) {
+    console.log('Ambos recorrentes');
+} else {
+    console.log(info['recorrente'],'e',info2['recorrente']);
 }
