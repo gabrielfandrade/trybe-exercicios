ef6220512043eacd1a7df802b3e56931d03d1fe0
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-03-10 13:45:08 -0300

    for/in e for/of - 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/pratica.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/pratica.js
@@ -1,13 +1,23 @@
-let pessoa = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: { golden: 2, silver: 3 },
-    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
-}
+// let pessoa = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: { golden: 2, silver: 3 },
+//     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+// }
 
 // console.log('A jogadora', pessoa['name'], pessoa['lastName'], 'tem', pessoa['age'], 'anos');
 
 // console.log('A jogadora', pessoa['name'], pessoa['lastName'] , 'foi eleita a melhor do mundo por', pessoa['bestInTheWorld'].length, 'vezes');
 
-console.log('A jogadora possui', pessoa['medals']['golden'], 'medalhas de ouro e', pessoa['medals']['silver'], 'medalhas de prata');
+// console.log('A jogadora possui', pessoa['medals']['golden'], 'medalhas de ouro e', pessoa['medals']['silver'], 'medalhas de prata');
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+for (let key in names) {
+    console.log('Olá', names[key]);
+}
