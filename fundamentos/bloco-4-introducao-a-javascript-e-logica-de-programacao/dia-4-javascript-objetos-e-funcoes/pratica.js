2b8b5604466a70a168637ce66ff2742f801cf5df
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-03-10 13:35:34 -0300

    Pratica 3 e 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/pratica.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/pratica.js
@@ -3,7 +3,9 @@ let pessoa = {
     lastName: 'Silva',
     age: 34,
     medals: { golden: 2, silver: 3 },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
 }
 
-console.log('A jogadora', pessoa['name'], pessoa['lastName'], 'tem', pessoa['age'], 'anos');
+// console.log('A jogadora', pessoa['name'], pessoa['lastName'], 'tem', pessoa['age'], 'anos');
 
+console.log('A jogadora', pessoa['name'], pessoa['lastName'] , 'foi eleita a melhor do mundo por', pessoa['bestInTheWorld'].length, 'vezes');
