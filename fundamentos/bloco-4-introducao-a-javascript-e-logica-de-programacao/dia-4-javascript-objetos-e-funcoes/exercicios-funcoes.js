3765bc596637a52312939797ac6fcee54195e703
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-03-10 15:05:31 -0300

    Funções 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-funcoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-funcoes.js
@@ -1,9 +1,24 @@
-function palindromo(string) {
-    if (string.split("").reverse().join("")==string) {
-        return true;
-    } else {
-        return false;
+// function palindromo(string) {
+//     if (string.split("").reverse().join("")==string) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(palindromo('arara'));
+
+function maiorNumero(numList) {
+    let maior = numList[0];
+
+    for (let number of numList) {
+        if (number > maior) {
+            maior = number;
+        }
     }
+
+    return maior;
+    
 }
 
-console.log(palindromo('arara'));
+console.log(maiorNumero([2, 3, 6, 7, 10, 1]));
