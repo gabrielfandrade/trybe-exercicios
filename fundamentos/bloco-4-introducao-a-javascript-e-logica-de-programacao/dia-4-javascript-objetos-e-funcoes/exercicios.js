c7f0a728be7d3abf3229a41ea54731eb16ef4fb1
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-03-10 14:50:43 -0300

    Exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,9 +1,9 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: 'Sim'
+//   };
 
 //   console.log('Bem-vinda,', info['personagem']);
 
@@ -17,18 +17,33 @@ let info = {
 //     console.log(info[key]);
 // }
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
-  };
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim'
+//   };
 
-console.log(info['personagem'],'e',info2['personagem']);
-console.log(info['origem'],'e',info2['origem']);
-console.log(info['nota'],'e',info2['nota']);
-if (info['recorrente']==info2['recorrente']) {
-    console.log('Ambos recorrentes');
-} else {
-    console.log(info['recorrente'],'e',info2['recorrente']);
-}
+// console.log(info['personagem'],'e',info2['personagem']);
+// console.log(info['origem'],'e',info2['origem']);
+// console.log(info['nota'],'e',info2['nota']);
+// if (info['recorrente']==info2['recorrente']) {
+//     console.log('Ambos recorrentes');
+// } else {
+//     console.log(info['recorrente'],'e',info2['recorrente']);
+// }
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
+    ],
+};
+
+console.log('O livro favorito de', leitor['nome'], leitor['sobrenome'], 'se chama', "'" + leitor['livrosFavoritos'][0]['titulo'] + "'");
