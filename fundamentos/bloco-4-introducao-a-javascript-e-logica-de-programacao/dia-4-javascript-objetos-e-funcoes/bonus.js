d735dc178ae25d285c533088f8ffacf128b04e65
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-03-10 18:27:16 -0300

    Bonus 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bonus.js
@@ -1,28 +1,44 @@
 
 /** Source: https://stackoverflow.com/questions/49659711/roman-to-integer-in-js-why-it-only-convert-the-first-character */
-const roman = new Map([
-    ['I', 1],
-    ['V', 5],
-    ['X', 10],
-    ['L', 50],
-    ['C', 100],
-    ['D', 500],
-    ['M', 1000],
-]);
+// const roman = new Map([
+//     ['I', 1],
+//     ['V', 5],
+//     ['X', 10],
+//     ['L', 50],
+//     ['C', 100],
+//     ['D', 500],
+//     ['M', 1000],
+// ]);
 
-function romanToInt(string) {
-    let result = 0,
-        current, previous = 0;
-    for (const char of string.split("").reverse()) {
-        current = roman.get(char);
-        if (current >= previous) {
-            result += current;
-        } else {
-            result -= current;
+// function romanToInt(string) {
+//     let result = 0,
+//         current, previous = 0;
+//     for (const char of string.split("").reverse()) {
+//         current = roman.get(char);
+//         if (current >= previous) {
+//             result += current;
+//         } else {
+//             result -= current;
+//         }
+//         previous = current;
+//     }
+//     return result;
+// }
+
+// console.log(romanToInt("CM"));
+
+let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+
+function arrayOfNumbers(vector) {
+    let pares = [];
+    for (const list of vector) {
+        for (const key in list) {
+            if (list[key] % 2 === 0) {
+                pares.push(list[key]);
+            }
         }
-        previous = current;
     }
-    return result;
+    return pares;
 }
 
-console.log(romanToInt("CM"));
+console.log(arrayOfNumbers(vector));
