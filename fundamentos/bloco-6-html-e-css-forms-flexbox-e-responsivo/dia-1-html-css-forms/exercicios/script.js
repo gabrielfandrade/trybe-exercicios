5a00bb3301e29a02b063e882b41c216b8439d9ab
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-03-24 15:01:28 -0300

    Bloco 6 - Dia 1 - Bonus 1

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
@@ -1,5 +1,18 @@
 const submit = document.querySelector('#form-button');
+const checkFoto = document.querySelector('#input-termo');
+const form = document.querySelector('#form');
 
 submit.addEventListener('click', function (event) {
   event.preventDefault();
-})
+});
+
+function verificar() {
+  submit.disabled = !checkFoto.checked;
+}
+
+window.onload = function() {
+  submit.addEventListener('click', function (event) {
+    event.preventDefault();
+  });
+  checkFoto.addEventListener('change', verificar);
+};
