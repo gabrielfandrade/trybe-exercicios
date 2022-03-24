8e52c0396ff1319def72e3a160b83341d1b3fc0c
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-03-24 15:05:32 -0300

    Bloco 6 - Dia 1 - Bonus 2

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
@@ -2,11 +2,35 @@ const submit = document.querySelector('#form-button');
 const checkFoto = document.querySelector('#input-termo');
 const form = document.querySelector('#form');
 
+function validacao() {
+  const email = document.querySelector('#input-email').value.length;
+  const invalidEmail = email < 10 || email > 50;
+
+  const name = document.querySelector('#input-nome').value.length;
+  const invalidName = name < 10 || name > 40;
+
+  const reason = document.querySelector('#input-texto').value.length;
+  const invalidReason = reason > 500;
+
+  if (invalidEmail || invalidName || invalidReason) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
 submit.addEventListener('click', function (event) {
   event.preventDefault();
+  const validar = validacao();
+  if (validar === false) {
+    alert('Dados inválidos');
+  } else {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  }
 });
 
-function verificar() {
+function verificarChecked() {
   submit.disabled = !checkFoto.checked;
 }
 
@@ -14,5 +38,5 @@ window.onload = function() {
   submit.addEventListener('click', function (event) {
     event.preventDefault();
   });
-  checkFoto.addEventListener('change', verificar);
+  checkFoto.addEventListener('change', verificarChecked);
 };
