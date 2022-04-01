eea6e532afc1f49b1a33b77dcd5ce34562c40012
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-04-01 18:08:46 -0300

    7.1 - Parte 2 - fatorial com ternary operator

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/script.js
@@ -1,3 +1,5 @@
+// Parte 1 
+// Exercicio 1
 const scope = escopo => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -11,6 +13,7 @@ const scope = escopo => {
 
 scope(true);
 
+// Exercicio 2
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 const criarOddsAndEvens = lista => lista.sort(function (a, b) {
@@ -18,3 +21,8 @@ const criarOddsAndEvens = lista => lista.sort(function (a, b) {
 });
 
 console.log(`Os números ${criarOddsAndEvens(oddsAndEvens)} se encontram ordenados de forma crescente !`);
+
+// Parte 2
+// Exercicio 1
+const fatorial = num => num > 1 ? num * fatorial(num - 1) : 1;
+console.log(fatorial(5));
