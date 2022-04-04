bfc0205fa9ec3536ef5df66913048dd0d1dec772
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-04-04 13:39:37 -0300

    7.2 - Object.assign

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/pratica/obeject-keys.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/pratica/obeject-keys.js
@@ -8,6 +8,16 @@ const pessoa = {
   age: 24,
 };
 
+const local = {
+  pais: 'Brasil',
+  estado: 'São Paulo',
+  cidade: 'São Paulo',
+}
+
 addKeys(pessoa, 'lastName', 'Andrade');
 
-console.log(Object.entries(pessoa));
+const nova = Object.assign({}, pessoa, local)
+
+console.log(pessoa);
+
+console.log(nova);
