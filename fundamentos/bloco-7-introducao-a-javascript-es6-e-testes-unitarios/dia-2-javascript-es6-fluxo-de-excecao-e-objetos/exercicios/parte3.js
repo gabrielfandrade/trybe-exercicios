1b6359281e1b08b301bb399688b502418bd282cd
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-04-04 15:42:44 -0300

    7.2 - Parte 3 - Listar keys de um objeto

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicios/parte3.js
@@ -0,0 +1,37 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function adicionaKey(obj, chave, valor) {
+  obj[chave] = valor;
+}
+
+adicionaKey(lesson2,'turno','noite');
+
+function keysObjeto(obj) {
+  console.log(Object.keys(obj));
+}
+
+keysObjeto(lesson2);
+
+function tamanhoObjeto(obj) {
+  console.log(Object.keys(obj).length);
+}
+
+tamanhoObjeto(lesson1);
