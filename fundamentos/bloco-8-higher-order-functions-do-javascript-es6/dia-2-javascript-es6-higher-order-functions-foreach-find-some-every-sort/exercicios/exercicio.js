79cce02a1f35d2a2a407d4addbc04255b2bdf9b2
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-04-08 14:49:53 -0300

    8.2 - Exercicio 3 - Livro com 26 caracteres

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios/exercicio.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios/exercicio.js
@@ -80,17 +80,6 @@ function smallerName() {
 }
 
 //Exercício 3
-const expectedResult = {
-  author: {
-    birthYear: 1948,
-    name: 'George R. R. Martin',
-  },
-  genre: 'Fantasia',
-  id: 1,
-  name: 'As Crônicas de Gelo e Fogo',
-  releaseYear: 1991,
-};
-
 function getNamedBook() {
-  // escreva seu código aqui
+  return books.find((book) => book.name.length === 26);
 }
