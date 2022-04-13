477349cffbb162999834169c583ff900e7ac822b
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-04-13 13:50:31 -0300

    8.5 - Array Destructuring

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixar/destructuring.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixar/destructuring.js
@@ -17,4 +17,26 @@ const userInfos = {
 
 const {name, age, nationality, profession, squad, squadInitials} = userInfos;
 
-console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
+// console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
+
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+// saudacoes[1](saudacoes[0]);
+
+const [saudacao, realizaSaudacao] = saudacoes;
+
+// console.log (realizaSaudacao(saudacao));
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[comida, animal, bebida] = [bebida, comida, animal];
+
+// console.log(comida, animal, bebida);
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares);
