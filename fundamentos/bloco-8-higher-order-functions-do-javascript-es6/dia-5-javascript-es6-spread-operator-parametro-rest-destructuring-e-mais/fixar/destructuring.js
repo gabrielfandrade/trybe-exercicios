49287a621b5c421c15944f0ea5a57d2361d5caa8
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-04-13 13:55:09 -0300

    8.5 - Default Destructuring

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixar/destructuring.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixar/destructuring.js
@@ -39,4 +39,20 @@ let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 
 [,,, ...numerosPares] = numerosPares;
 
-console.log(numerosPares);
+// console.log(numerosPares);
+
+const getNationality = ({ firstName, nationality = 'Brazilian'}) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
