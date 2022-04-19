56ca8ed1694c4cd2f18dab189ebbe1b64d03a61f
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-04-19 14:31:36 -0300

    9.1 - Exercicio 4 - Message delay

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios/script.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios/script.js
@@ -1,3 +1,4 @@
+// Exercício 3
 const getPlanet = () => {
   const mars = {
     name: "Mars",
@@ -11,4 +12,22 @@ const getPlanet = () => {
   }, 4000);
 };
 
-getPlanet(); // imprime Marte depois de 4 segundos
+// getPlanet(); // imprime Marte depois de 4 segundos
+
+// Exercício 4
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = (callback) => {
+  setTimeout(() => {
+    const message = `Mars temperature is: ${callback()} degree Celsius`;
+    console.log(message);
+  }, messageDelay());
+};
+
+sendMarsTemperature(getMarsTemperature); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
