9c2d7b459c1bc75b27d2a95d98c1c84658babf28
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-04-19 15:07:55 -0300

    9.1 - Exercicio 9 - Testes assincronos

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios/pokemonGen1.test.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios/pokemonGen1.test.js
@@ -1,1 +1,27 @@
 // Exercício 9
+// Verifique se a importação do arquivo correto está sendo feita.
+const { getPokemonDetails } = require("./pokemonGen1");
+
+describe("A função getPokemonDetails", () => {
+  it("retorna erro quando procuramos um pokemon que não existe no banco de dados", (done) => {
+    const expected = new Error('Não temos esse pokémon para você :(');
+
+    const callback = (error, result) => {
+      expect(error).toEqual(expected);
+      done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Giratina', callback);
+  });
+
+  it("retorna um pokemon que existe no banco de dados", (done) => {
+    const expected = 'Olá, seu pokémon é o Squirtle, o tipo dele é Water e a habilidade dele é Rain Dish'
+
+    const callback = (error, result) => {
+      expect(result).toBe(expected);
+      done();
+    };
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Squirtle', callback);
+  });
+});
