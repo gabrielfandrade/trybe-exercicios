b171d2dc42bfe01ddb53c613f8fa0e07fd03827a
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-05-16 15:10:39 -0300

    12.1 - Renderização condicional 2

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/fixar/lifecycleapp/src/components/PersonDetails.js
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+import Loading from './Loading';
+import PersonCard from './PersonCard';
+
+class PersonDetails extends Component {
+  state = {
+    loading: true,
+    person: [],
+  };
+
+  componentDidMount = () => {
+    const url = 'https://api.randomuser.me/';
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          person: data.results,
+          loading: false,
+        })
+      })
+  }
+
+  shouldComponentUpdate = (_nextProps, nextState) => {
+    const AGE = 50;
+    if (nextState.person[0].dob.age > AGE) {
+      return false;
+    }
+    return true;
+  }
+
+  getUserElements = user => ({
+    name: `${user.name.first} ${user.name.last}`,
+    email: user.email,
+    age: user.dob.age,
+    image: user.picture.thumbnail,
+  })
+
+  render() {
+    const { person, loading } = this.state;
+
+    if (loading) return <Loading />;
+
+    return (
+      <div>
+        {person.map((currentPerson, index) => (
+          <PersonCard
+            key={index}
+            person={this.getUserElements(currentPerson)}
+          />))}
+      </div>
+    )
+  }
+}
+
+export default PersonDetails;
