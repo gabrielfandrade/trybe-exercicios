ea1b13c742cb349761a221d8e00e516c2d905d78
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-06-08 13:59:19 -0300

    15.1 - Exercicio 1.6 - Random Color

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercicios/script.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercicios/script.js
@@ -1,9 +1,17 @@
-const Redux = require('redux');
-
 const INITIAL_STATE = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
+
+function criarCor() {
+  const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let cor = '#';
+  const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+  for (let i = 0; i < 6; i += 1) {
+      cor += oneChar[aleatorio()];
+  }
+  return cor;
+}
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
@@ -16,6 +24,12 @@ const reducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
+      };
+    case 'RANDOM_COLOR': 
+      return {
+        ...state,
+        colors: [...state.colors, criarCor()],
+        index: state.colors.length,
       };
     default:
       return state;
@@ -30,6 +44,9 @@ document.getElementById('next').addEventListener('click', () => {
 document.getElementById('previous').addEventListener('click', () => {
   store.dispatch({ type: 'PREVIOUS_COLOR' });
 });
+document.getElementById('random').addEventListener('click', () => {
+  store.dispatch({ type: 'RANDOM_COLOR' });
+})
 
 store.subscribe(() => {
   const { colors, index } = store.getState();
