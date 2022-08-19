8dcea9ea66a5ae0636ffe6aaf33c4613d6f4bc84
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-08-19 12:32:19 -0300

    22.1 - Exercício 5

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicios/bmi.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicios/bmi.js
@@ -6,10 +6,15 @@ const bmi = (weight, height) => {
 
   const result = weight / heightSquare;
 
-  return result;
+  if (result < 18.5) console.log('Abaixo do peso (magreza)');
+  if (result >= 18.5 && result <= 24.9) console.log('Peso normal');
+  if (result >= 25.0 && result <= 29.9) console.log('Acima do peso (sobrepeso)');
+  if (result >= 30.0 && result <= 34.9) console.log('Obesidade grau I');
+  if (result >= 35.0 && result <= 39.9) console.log('Obesidade grau II');
+  if (result >= 40.0) console.log('Obesidade graus III e IV');
 }
 
 const weight = readline.questionFloat('What\'s your weight? (kg) ');
 const height = readline.questionInt('What\'s your height? (cm) ');
 
-console.log(bmi(weight, height));
+bmi(weight, height);
